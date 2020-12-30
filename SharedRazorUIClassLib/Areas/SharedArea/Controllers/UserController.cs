7e460a8b6321446f975f8907f603a1bb539de895
Agah Shamekhi <Agah.Shamekhi@gmail.com>
2020-12-31 02:05:00 +0330

    Add project files.

---
+++ SharedRazorUIClassLib/Areas/SharedArea/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharedRazorUIClassLib.Areas.SharedArea.Controllers
+{
+    [Area("SharedArea")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OverrideExample()
+        {
+            return View();
+        }
+    }
+}
