3e1a308c2c5d295ab10b88062d303047a59fb118
Agah Shamekhi <Agah.Shamekhi@gmail.com>
2020-12-31 03:04:11 +0330

    Shared Resources

---
+++ SharedRazorUIClassLib/UIConfigureOptions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedRazorUIClassLib
+{
+    public class UIConfigureOptions : IPostConfigureOptions<StaticFileOptions>
+    {
+        public UIConfigureOptions(Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+        public Microsoft.AspNetCore.Hosting.IHostingEnvironment Environment { get; }
+
+        public void PostConfigure(string name, StaticFileOptions options)
+        {
+            name = name ?? throw new ArgumentNullException(nameof(name));
+            options = options ?? throw new ArgumentNullException(nameof(options));
+
+            options.ContentTypeProvider ??= new FileExtensionContentTypeProvider();
+
+            if (options.FileProvider == null && Environment.WebRootFileProvider == null)
+            {
+                throw new InvalidOperationException("Missing FileProvider.");
+            }
+
+            options.FileProvider = options.FileProvider ?? Environment.WebRootFileProvider;
+
+            var basePath = "wwwroot";
+
+            var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, basePath);
+            options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
+        }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SharedRazorUIClassLib;
 
 namespace WebApplication1
 {
@@ -17,6 +18,7 @@ namespace WebApplication1
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureOptions(typeof(UIConfigureOptions));
             services.AddControllersWithViews();
         }
 
